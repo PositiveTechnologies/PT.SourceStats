9fb15b1326ef9e05b53deb12037b9aacdc167e58
Ivan Kochurkin <kvanttt@gmail.com>
2019-04-20 22:49:02 +0300

    Update framwork net462 -> net472

--- AssemblyInfoCommon.cs
+++ AssemblyInfoCommon.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("Positive Technologies")]
 [assembly: AssemblyProduct("PT.SourceStats")]
-[assembly: AssemblyCopyright("Copyright © Positive Technologies 2016-2018")]
+[assembly: AssemblyCopyright("Copyright © Positive Technologies 2016-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(false)]
